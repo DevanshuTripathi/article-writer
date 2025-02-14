5c6b0b5dfd418a2d179b4c99fc9631d226e56618
DevanshuTripathi120 <aditheprince120@gmail.com>
2025-02-14 20:00:01 +0530

    cors

--- server/article_writer/settings.py
+++ server/article_writer/settings.py
@@ -55,9 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
+CORS_ALLOWED_ORIGINS = ['*']
 
 ROOT_URLCONF = 'article_writer.urls'
 
